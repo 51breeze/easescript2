e6efb028570768c6053b2fe74ec27526943aa517
yejun123 <664371281@qq.com>
2021-03-09 18:36:25 +0800

    dev

---
+++ service.js
@@ -0,0 +1,33 @@
+const Compiler = require("./lib/Compiler");
+const options = {service:true,locations:true};
+class Service{
+    set options( value ){
+        this._options = Object.assign({service:true,locations:true},options,value);
+    }
+    get options(){
+        return this._options || options;
+    }
+    get compiler(){
+        return this._compiler || (this._compiler = new Compiler(options));
+    }
+    getModule(file){
+        file = this.compiler.getFileAbsolute(file);
+        const module = this.compiler.modules.get( file );
+        if( !module ){
+            const compilation = this.compiler.createCompilation( this.compiler.createModule( file ) );
+            compilation.parser();
+            return compilation.module;
+        }
+        return module;
+    }
+    start(action, file, startAt, token){
+        const module = this.getModule(file);
+        const stack = module.stackNodeMap.get(startAt);
+        if( stack ){
+            return stack.value();
+        }
+        return null;
+    }
+}
+
+module.exports = Service;
